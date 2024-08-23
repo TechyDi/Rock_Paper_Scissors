c6efd0d92e498ee4daf4631b2decb0cd7bd12e95
TechyDi <dkd1210202gmail.com>
2024-08-23 21:34:23 +0530

    optimization

--- RPS.java
+++ RPS.java
@@ -1,36 +1,54 @@
 import java.util.*;
-
 public class RPS {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
     
         System.out.println("\nWELCOME TO THE Rock_Paper_Scissors GAME");
-        System.out.println("How many rounds of Rock_Paper_Scissors would you like to play?");
+        System.out.println("\nHow many rounds of Rock_Paper_Scissors would you like to play?");
 
-        int round = Integer.parseInt(sc.nextLine());
-        int x = 0;
-        while(x<round){
-            playRPS(sc);
-            x++;
-        }
+            int round = Integer.parseInt(sc.nextLine());
+                int x = 0;
+                int playersWinscount = 0;
+                while(x<round){
+                boolean playerwon = playRPS(sc);
+                    if(playerwon){
+                        playersWinscount++;
+                    }
+                x++;
+                }
         
     }
-
-    static void playRPS(Scanner sc){
+    //GAME START
+    static Boolean playRPS(Scanner sc){
 
         //INPUT USER
-
-        System.out.println("\nMake a move! (rock/paper/scissors)");
-        String playerMove = sc.nextLine();
+    String playerMove;
+    while (true) {
         
+        System.out.println("\nMake a move! (rock/paper/scissors)");
+            playerMove = sc.nextLine();
+
+        //CHECK FOR VALID INPUT
+        
+        if (playerMove.equals("rock") || playerMove.equals("paper") || playerMove.equals("scissors")) {
+            break;
+        } else {
+            System.out.println("\nINVALID INPUT. PLEASE TRY AGAIN.");
+        }
+    }
+       
         //COMPUTER INPUT
+
         Random random = new Random();
         int randomNum = random.nextInt(3);
+        
         //CHECKING THE RANDOMNESS
+
         System.out.println(randomNum);
 
-            //Assigning the value to ComputerMove
+        //Assigning the value to ComputerMove
+
         String computerMove;
         if(randomNum==0){
             computerMove = "rock";
@@ -39,15 +57,19 @@ public class RPS {
         }else {
             computerMove = "Scissors";
         }
-        System.out.println("Computer Choose "+ computerMove + "!");
+            System.out.println("Computer Choose "+ computerMove + "!");
 
         // RESULTS
+
         if(playerMove.equalsIgnoreCase(computerMove)){
             System.out.println("AWWWW ! It's A Draw.");
+            return false;
         }else if(playerWins(playerMove, computerMove)){
             System.out.println("HURRAY ! You Wins.");
+            return true;
         }else{
             System.out.println("You Lost Buddy. Lets's Try Again. ");
+            return false;
         }
     }
 
